9b28fdef5a0678d84ad2cb3626882b33cc111398
Mampuru <mampuruntwampe@gmail.com>
2024-02-07 22:58:35 +0200

    Fixed token property type for const to var

--- lib/api/api_config.dart
+++ lib/api/api_config.dart
@@ -2,4 +2,4 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const baseUrl = "https://api.laws.africa/v2/akn/za-cpt/.json";
-const authToken = dotenv.env['API_KEY'];
+var authToken = dotenv.env['API_KEY'];
