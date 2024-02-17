77d83cb7b7861e7c399d23f0766514ebc3b3eef5
Mampuru <mampuruntwampe@gmail.com>
2024-02-17 23:55:48 +0200

    Update responseDate functionality

--- lib/controllers/legislation_controller.dart
+++ lib/controllers/legislation_controller.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import '../api/api_config.dart';
+import '../constants.dart';
 import '../models/legislation_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -29,29 +30,32 @@ class LegislationController extends GetxController {
           'Authorization': 'Token $token',
         },
       );
-
       // Check if the request was successful
       if (response.statusCode == 200) {
         // Parse response and populate legislationList
-        final List<dynamic> responseData = json.decode(response.body);
-        final List<Legislation> parsedLegislationList = responseData
-            .map((item) => Legislation(
-          title: item['title'] ?? '',
-          description: item['description'] ?? '',
-          status: item['status'] ?? '',
-        ))
-            .toList();
+        final Map<dynamic,dynamic> responseData = json.decode(response.body);
+        logger.i(responseData["results"]);
+        List<dynamic> parsedLegislationList = responseData["results"];
+        logger.i(parsedLegislationList);
+        // final List<Legislation> parsedLegislationList = responseData
+        //     .map((item) => Legislation(
+        //   title: item['title'] ?? '',
+        //   description: item['description'] ?? '',
+        //   status: item['status'] ?? '',
+        // )).toList();
 
         // Update legislationList with parsed data
-        legislationList(parsedLegislationList);
+        // legislationList(parsedLegislationList);
       } else {
         // Handle error if the request was not successful
         if (kDebugMode) {
+          logger.e(response.statusCode);
           print('Failed to fetch legislation: ${response.statusCode}');
         }
       }
     } catch (error) {
-      // Handle any exceptions that occur during the API call
+      // Handle any exceptions that occur during the API callz
+      logger.e(error);
       if (kDebugMode) {
         print('Error fetching legislation: $error');
       }
