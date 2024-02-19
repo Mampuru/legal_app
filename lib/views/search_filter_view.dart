b6d8f4d14f9458e906760269f134322804d2d8bc
Mampuru <mampuruntwampe@gmail.com>
2024-02-19 23:31:49 +0200

    Basic template code for the view

---
+++ lib/views/search_filter_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchFilterView extends StatelessWidget {
+  const SearchFilterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
