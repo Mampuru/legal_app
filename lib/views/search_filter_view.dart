81988236ee0ff0921639f844a66748564d2bd0bc
Mampuru <mampuruntwampe@gmail.com>
2024-02-23 01:35:23 +0200

    Fixed the layout errors on the widget

--- lib/views/search_filter_view.dart
+++ lib/views/search_filter_view.dart
@@ -1,47 +1,53 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:legal_app/views/search_filter_comp.dart';
 
-class SearchFilterView extends StatelessWidget {
+class SearchFilterView extends StatefulWidget {
   const SearchFilterView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<SearchFilterView> createState() => _SearchFilterViewState();
+}
+
+class _SearchFilterViewState extends State<SearchFilterView> {
     String? selectedCountry;
     String? selectedTopic;
 
-    void _onCountryChanged(String? value) {
+    void onCountryChanged(String? value) {
       setState(() {
         selectedCountry = value;
       });
     }
 
-    void _onTopicChanged(String? value) {
+    void onTopicChanged(String? value) {
       setState(() {
         selectedTopic = value;
       });
     }
 
-    void _onSearchPressed() {
+    void onSearchPressed() {
       // Implement your search functionality here
-      print('Searching for $selectedCountry - $selectedTopic');
+      if (kDebugMode) {
+        print('Searching for $selectedCountry - $selectedTopic');
+      }
     }
 
     @override
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
-          title: Text('Your Screen Title'),
+          title: const Text('Your Screen Title'),
         ),
         body: Column(
           children: [
             SearchAndFilter(
-              countries: ['Country 1', 'Country 2', 'Country 3'],
-              topics: ['Topic 1', 'Topic 2', 'Topic 3'],
+              countries: const ['Country 1', 'Country 2', 'Country 3'],
+              topics: const ['Topic 1', 'Topic 2', 'Topic 3'],
               selectedCountry: selectedCountry,
               selectedTopic: selectedTopic,
-              onCountryChanged: _onCountryChanged,
-              onTopicChanged: _onTopicChanged,
-              onSearchPressed: _onSearchPressed,
+              onCountryChanged: onCountryChanged,
+              onTopicChanged: onTopicChanged,
+              onSearchPressed: onSearchPressed,
             ),
             // Your other widgets here
           ],
