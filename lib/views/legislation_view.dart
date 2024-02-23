600fbe0ed92fd864fb6a08e32985afa16e783a32
Mampuru <mampuruntwampe@gmail.com>
2024-02-23 03:03:38 +0200

    Removed unused mode

--- lib/views/legislation_view.dart
+++ lib/views/legislation_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../controllers/legislation_controller.dart';
-import '../models/legislation_model.dart';
 
 // ignore: use_key_in_widget_constructors
 class LegislationView extends StatelessWidget {
