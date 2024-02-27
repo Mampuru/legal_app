da6634758c4669042207cc4f8eb20482b9b7453c
Mampuru <mampuruntwampe@gmail.com>
2024-02-27 23:58:49 +0200

    Addition for fetch countries endpoint

--- lib/controllers/legislation_controller.dart
+++ lib/controllers/legislation_controller.dart
@@ -63,4 +63,50 @@ class LegislationController extends GetxController {
     }
   }
 
+
+  void fetchCounties() async {
+    try {
+      isLoading(true);
+      // Make API call to Law.Africa API
+      final url = Uri.parse('$baseUrl/countries.json');
+      var token = authToken; // Replace 'your_auth_token_here' with your actual token
+
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Token $token',
+        },
+      );
+      // Check if the request was successful
+      if (response.statusCode == 200) {
+        // Parse response and populate legislationList
+        final Map<dynamic,dynamic> responseData = json.decode(response.body);
+        logger.i(responseData["results"]);
+        // List<dynamic> responseList = responseData["results"];
+        //
+        // final List<Legislation> parsedLegislationList = responseList
+        //     .map((item) => Legislation(
+        //   title: item['title'] ?? '',
+        //   description: item['updated_at'] ?? '',
+        //   status: item['publication_name'] ?? '',
+        // )).toList();
+
+        // Update legislationList with parsed data
+        // legislationList(parsedLegislationList);
+      } else {
+        // Handle error if the request was not successful
+        if (kDebugMode) {
+          print('Failed to fetch legislation: ${response.statusCode}');
+        }
+      }
+    } catch (error) {
+      // Handle any exceptions that occur during the API callz
+      if (kDebugMode) {
+        print('Error fetching legislation: $error');
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+
 }
