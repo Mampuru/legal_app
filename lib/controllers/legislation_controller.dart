f82839cc344f188ddea622b45853d3426b76729e
Mampuru <mampuruntwampe@gmail.com>
2024-02-29 23:44:30 +0200

    Addition of the fetch toc function

--- lib/controllers/legislation_controller.dart
+++ lib/controllers/legislation_controller.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:legal_app/models/country_model.dart';
+import 'package:legal_app/models/toc_model.dart';
 import '../api/api_config.dart';
 import '../constants.dart';
 import '../models/legislation_model.dart';
@@ -12,6 +13,7 @@ class LegislationController extends GetxController {
   var isLoading = true.obs;
   var legislationList = <Legislation>[].obs;
   var countryList = <Country>[].obs;
+  var tocList = <TOC>[].obs;
 
   @override
   void onInit() {
@@ -20,6 +22,7 @@ class LegislationController extends GetxController {
   }
 
   void fetchLegislation() async {
+    //TODO make the country variable a parameter
     String country = "za-cpt";
     try {
       isLoading(true);
@@ -112,4 +115,50 @@ class LegislationController extends GetxController {
     }
   }
 
+  void fetchTableOfContent() async {
+    String country = "za-cpt";
+    try {
+      isLoading(true);
+      final url = Uri.parse('$baseUrl/$country/toc.json');
+      var token = authToken;
+
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Token $token',
+        },
+      );
+      // Check if the request was successful
+      if (response.statusCode == 200) {
+        // Parse response and populate CountryList
+        final Map<dynamic,dynamic> responseData = json.decode(response.body);
+        logger.i(responseData["results"]);
+        List<dynamic> responseList = responseData["results"];
+
+        // final List<TOC> parsedTOCList = responseList
+        //     .map((item) => Country(
+        //   name: item['name'] ?? '',
+        //   code: item['code'] ?? '',
+        //   localities: item['localities'] ?? '',
+        // )).toList();
+
+        // Update CountryList with parsed data
+        tocList(parsedTOCList);
+        logger.i(countryList.length);
+      } else {
+        // Handle error if the request was not successful
+        if (kDebugMode) {
+          print('Failed to fetch legislation: ${response.statusCode}');
+        }
+      }
+    } catch (error) {
+      // Handle any exceptions that occur during the API callz
+      if (kDebugMode) {
+        print('Error fetching legislation: $error');
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
 }
+
