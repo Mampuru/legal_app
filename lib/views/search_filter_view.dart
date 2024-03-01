ab14af3f1d99a14bf19dee759776e17ca253325d
Mampuru <mampuruntwampe@gmail.com>
2024-03-01 23:40:01 +0200

    Fixed binding of list of countries data to dropdown

--- lib/views/search_filter_view.dart
+++ lib/views/search_filter_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:legal_app/views/search_filter_comp.dart';
 
 import '../controllers/legislation_controller.dart';
+import '../models/country_model.dart';
+
 
 class SearchFilterView extends StatefulWidget {
   const SearchFilterView({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class SearchFilterView extends StatefulWidget {
 
 class _SearchFilterViewState extends State<SearchFilterView> {
     final LegislationController _controller = Get.put(LegislationController());
+    late final List<Country> countries; // List of available countries
     String? selectedCountry;
     String? selectedTopic;
 
@@ -47,15 +49,46 @@ class _SearchFilterViewState extends State<SearchFilterView> {
       return Scaffold(
         body: Column(
           children: [
-            Obx(() => SearchAndFilter(
-              countries: _controller.countryList,
-              topics: _controller.tocList,
-              selectedCountry: selectedCountry,
-              selectedTopic: selectedTopic,
-              onCountryChanged: onCountryChanged,
-              onTopicChanged: onTopicChanged,
-              onSearchPressed: onSearchPressed,
-            )),
+            // Country dropdown
+            Obx(() =>
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  labelText: 'Country',
+                  border: OutlineInputBorder(),
+                ),
+                value: selectedCountry,
+                onChanged: onCountryChanged,
+                items: _controller.countryList.map((country) {
+                  return DropdownMenuItem<String>(
+                    value: country.name,
+                    child: Text(country.name),
+                  );
+                }).toList(),
+              ),
+            ),
+
+            const SizedBox(height: 16.0),
+            // Topic dropdown
+            // DropdownButtonFormField<String>(
+            //   decoration: const InputDecoration(
+            //     labelText: 'Topic',
+            //     border: OutlineInputBorder(),
+            //   ),
+            //   value: selectedTopic,
+            //   onChanged: onTopicChanged,
+            //   items: topics.map((topic) {
+            //     return DropdownMenuItem<String>(
+            //       value: topic.title,
+            //       child: Text(topic.title),
+            //     );
+            //   }).toList(),
+            // ),
+            const SizedBox(height: 16.0),
+            // Search button
+            ElevatedButton(
+              onPressed: onSearchPressed,
+              child: const Text('Search'),
+            ),
           ],
         ),
       );
