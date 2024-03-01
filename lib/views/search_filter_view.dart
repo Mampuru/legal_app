10cbc6f8777073556ed434ca49426bfae66611c5
Mampuru <mampuruntwampe@gmail.com>
2024-03-01 23:51:47 +0200

    Fixed layout for country dropdown list

--- lib/views/search_filter_view.dart
+++ lib/views/search_filter_view.dart
@@ -15,7 +15,6 @@ class SearchFilterView extends StatefulWidget {
 
 class _SearchFilterViewState extends State<SearchFilterView> {
     final LegislationController _controller = Get.put(LegislationController());
-    late final List<Country> countries; // List of available countries
     String? selectedCountry;
     String? selectedTopic;
 
@@ -47,49 +46,53 @@ class _SearchFilterViewState extends State<SearchFilterView> {
     @override
     Widget build(BuildContext context) {
       return Scaffold(
-        body: Column(
-          children: [
-            // Country dropdown
-            Obx(() =>
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(
-                  labelText: 'Country',
-                  border: OutlineInputBorder(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Country dropdown
+              Obx(() =>
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(
+                    labelText: 'Country',
+                    border: OutlineInputBorder(),
+                  ),
+                  value: selectedCountry,
+                  onChanged: onCountryChanged,
+                  items: _controller.countryList.map((country) {
+                    return DropdownMenuItem<String>(
+                      value: country.name,
+                      child: Text(country.name),
+                    );
+                  }).toList(),
                 ),
-                value: selectedCountry,
-                onChanged: onCountryChanged,
-                items: _controller.countryList.map((country) {
-                  return DropdownMenuItem<String>(
-                    value: country.name,
-                    child: Text(country.name),
-                  );
-                }).toList(),
               ),
-            ),
 
-            const SizedBox(height: 16.0),
-            // Topic dropdown
-            // DropdownButtonFormField<String>(
-            //   decoration: const InputDecoration(
-            //     labelText: 'Topic',
-            //     border: OutlineInputBorder(),
-            //   ),
-            //   value: selectedTopic,
-            //   onChanged: onTopicChanged,
-            //   items: topics.map((topic) {
-            //     return DropdownMenuItem<String>(
-            //       value: topic.title,
-            //       child: Text(topic.title),
-            //     );
-            //   }).toList(),
-            // ),
-            const SizedBox(height: 16.0),
-            // Search button
-            ElevatedButton(
-              onPressed: onSearchPressed,
-              child: const Text('Search'),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              // Topic dropdown
+              // DropdownButtonFormField<String>(
+              //   decoration: const InputDecoration(
+              //     labelText: 'Topic',
+              //     border: OutlineInputBorder(),
+              //   ),
+              //   value: selectedTopic,
+              //   onChanged: onTopicChanged,
+              //   items: topics.map((topic) {
+              //     return DropdownMenuItem<String>(
+              //       value: topic.title,
+              //       child: Text(topic.title),
+              //     );
+              //   }).toList(),
+              // ),
+              const SizedBox(height: 16.0),
+              // Search button
+              ElevatedButton(
+                onPressed: onSearchPressed,
+                child: const Text('Search'),
+              ),
+            ],
+          ),
         ),
       );
   }
