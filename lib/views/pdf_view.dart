1d38adaa16682de28d254062cf8f39a73350f759
Mampuru <mampuruntwampe@gmail.com>
2024-03-07 23:28:46 +0200

    Addition of pdf view

---
+++ lib/views/pdf_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PDFView extends StatefulWidget {
+  const PDFView({Key? key}) : super(key: key);
+
+  @override
+  State<PDFView> createState() => _PDFViewState();
+}
+
+class _PDFViewState extends State<PDFView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
