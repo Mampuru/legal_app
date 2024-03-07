89aa43bb996942c3feb17127ece187560f8565f3
Mampuru <mampuruntwampe@gmail.com>
2024-03-07 23:46:17 +0200

    Addition of the create PDF url function

--- lib/views/pdf_view.dart
+++ lib/views/pdf_view.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class PDFView extends StatefulWidget {
@@ -8,8 +12,52 @@ class PDFView extends StatefulWidget {
 }
 
 class _PDFViewState extends State<PDFView> {
+  String remotePDFpath = ""; //holder the pdf url from the object
+
+  @override
+  void initState() {
+    super.initState();
+
+    createFileOfPdfUrl().then((f) {
+      setState(() {
+        remotePDFpath = f.path;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Future<File> createFileOfPdfUrl() async {
+    Completer<File> completer = Completer();
+    if (kDebugMode) {
+      print("Start download file from internet!");
+    }
+    try {
+      // "https://berlin2017.droidcon.cod.newthinking.net/sites/global.droidcon.cod.newthinking.net/files/media/documents/Flutter%20-%2060FPS%20UI%20of%20the%20future%20%20-%20DroidconDE%2017.pdf";
+      // final url = "https://pdfkit.org/docs/guide.pdf";
+      const url = "http://www.pdf995.com/samples/pdf.pdf";
+      final filename = url.substring(url.lastIndexOf("/") + 1);
+      var request = await HttpClient().getUrl(Uri.parse(url));
+      var response = await request.close();
+      var bytes = await consolidateHttpClientResponseBytes(response);
+      var dir = await getApplicationDocumentsDirectory();
+      if (kDebugMode) {
+        print("Download files");
+      }
+      if (kDebugMode) {
+        print("${dir.path}/$filename");
+      }
+      File file = File("${dir.path}/$filename");
+
+      await file.writeAsBytes(bytes, flush: true);
+      completer.complete(file);
+    } catch (e) {
+      throw Exception('Error parsing asset file!');
+    }
+
+    return completer.future;
   }
 }
