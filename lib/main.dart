ef206f711c44216c902312e76068fcca2a9a1079
Mampuru <mampuruntwampe@gmail.com>
2024-03-31 16:50:26 +0200

    Removed whitespace

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:legal_app/views/home_view.dart';
-import 'package:legal_app/views/legislation_view.dart';
 
 import 'constants.dart';
 
@@ -30,4 +29,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
